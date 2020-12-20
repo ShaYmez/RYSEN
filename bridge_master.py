32c25e306c2c84cbc6f509089f67334f3a6553e1
Simon <simon@adlem.net>
2020-12-20 21:48:48 +0000

    Revert "Back off logging for dedup code"
    This reverts commit 4a2bdaf80f453d344ce9aa5a99e31f61fbe912ce.

--- bridge_master.py
+++ bridge_master.py
@@ -787,8 +787,7 @@ class routerOBP(OPENBRIDGE):
                 _target_status = systems[_target['SYSTEM']].STATUS
                 _target_system = self._CONFIG['SYSTEMS'][_target['SYSTEM']]
                 if (_target['SYSTEM'],_target['TS']) in _sysIgnore:
-                    if (_stream_id not in _target_status):
-                        logger.debug("(DEDUP) OBP Source Skipping system %s TS: %s",_target['SYSTEM'],_target['TS'])
+                    logger.debug("(DEDUP) OBP Source Skipping system %s TS: %s",_target['SYSTEM'],_target['TS'])
                     continue
                 if _target_system['MODE'] == 'OPENBRIDGE':
                     if _noOBP == True:
@@ -1058,9 +1057,9 @@ class routerHBP(HBSYSTEM):
                 if _target['ACTIVE']:
                     _target_status = systems[_target['SYSTEM']].STATUS
                     _target_system = self._CONFIG['SYSTEMS'][_target['SYSTEM']]
+
                     if (_target['SYSTEM'],_target['TS']) in _sysIgnore:
-                        if (_stream_id not in _target_status):
-                            logger.debug("(DEDUP) HBP Source - Skipping system %s TS: %s",_target['SYSTEM'],_target['TS'])
+                        logger.debug("(DEDUP) HBP Source - Skipping system %s TS: %s",_target['SYSTEM'],_target['TS'])
                         continue
                     if _target_system['MODE'] == 'OPENBRIDGE':
                         if _noOBP == True:
