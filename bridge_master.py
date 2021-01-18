4fa05fb50389d21dbee72d25e3733d6805de7a25
Simon <simon@adlem.net>
2021-01-18 17:02:09 +0000

    tidy up stream trimmer logging

--- bridge_master.py
+++ bridge_master.py
@@ -410,7 +410,7 @@ def stream_trimmer_loop():
                     if systems[system].STATUS[stream_id]['LAST'] < _now - 5:
                         remove_list.append(stream_id)
                 except:
-                    logger.debug("(%s) Keyerror - stream trimmer %s %s %s %s",system,systems[system].STATUS[stream_id]['START'],systems[system].STATUS[stream_id]['CONTENTION'],systems[system].STATUS[stream_id]['RFS'],int_id(systems[system].STATUS[stream_id]['TGID']))
+                    logger.debug("(%s) Keyerror - stream trimmer Stream ID: %s Start: %s Contention: %s RFS: %s  TGID: %s",system,systems[system].STATUS[stream_id],systems[system].STATUS[stream_id]['START'],systems[system].STATUS[stream_id]['CONTENTION'],systems[system].STATUS[stream_id]['RFS'],int_id(systems[system].STATUS[stream_id]['TGID']))
                     systems[system].STATUS[stream_id]['LAST'] = _now
                     continue
                 
