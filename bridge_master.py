6ad5af1b2ba56806085a61ee79714c572e20abe7
Simon <simon@adlem.net>
2021-02-23 15:31:58 +0000

    Don't allow decimal point / full stop in TG in options

--- bridge_master.py
+++ bridge_master.py
@@ -553,13 +553,13 @@ def options_config():
                     
                 if _options['TS1_STATIC']:
                     re.sub("\s","",_options['TS1_STATIC'])
-                    if re.search("![\d\,\.]",_options['TS1_STATIC']):
+                    if re.search("![\d\,]",_options['TS1_STATIC']):
                         logger.debug('(OPTIONS) %s - TS1_STATIC contains characters other than numbers and comma, ignoring',_system)
                         continue
                 
                 if _options['TS2_STATIC']:
                     re.sub("\s","",_options['TS2_STATIC'])
-                    if re.search("![\d\,\.]",_options['TS2_STATIC']):
+                    if re.search("![\d\,]",_options['TS2_STATIC']):
                         logger.debug('(OPTIONS) %s - TS2_STATIC contains characters other than numbers and comma, ignoring',_system)
                         continue
                 
