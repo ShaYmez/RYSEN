19965158ae3c838d086d213d70d292d882b2f28a
Simon <simon@gb7fr.org.uk>
2021-04-03 17:10:24 +0100

    Anothet T

--- hblink.py
+++ hblink.py
@@ -543,7 +543,7 @@ class HBSYSTEM(DatagramProtocol):
 
                     self.send_peer(_peer_id, b''.join([RPTACK, _peer_id]))
                     logger.info('(%s) Peer %s (%s) has sent repeater configuration, Package ID: %s, Software ID: %s', self._system, _this_peer['CALLSIGN'], _this_peer['RADIO_ID'],self._peers[_peer_id]['PACKAGE_ID'],self._peers[_peer_id]['SOFTWARE_ID'])
-                else
+                else:
                     self.transport.write(b''.join([MSTNAK, _peer_id]), _sockaddr)
                     logger.warning('(%s) Peer info from Radio ID that has not logged in: %s', self._system, int_id(_peer_id))
 
