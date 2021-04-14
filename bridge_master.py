1cf241532122e2a9763cbcbc3205f8fba8f02719
Simon <simon@gb7fr.org.uk>
2021-04-14 22:36:28 +0100

    Handle BCSQ when coming from a HBP source also

--- bridge_master.py
+++ bridge_master.py
@@ -1457,6 +1457,10 @@ class routerHBP(HBSYSTEM):
                             continue
                         #We want to ignore this system and TS combination if it's called again for this packet
                         _sysIgnore.append((_target['SYSTEM'],_target['TS']))
+                        
+                        if ('_bcsq' in _target_system) and (_dst_id in _target_system['_bcsq']) and (_target_system['_bcsq'][_target['TGID'] == _stream_id):
+                        #logger.info('(%s) Conference Bridge: %s, is Source Quenched for Stream ID: %s, skipping system: %s TS: %s, TGID: %s', self._system, _bridge, int_id(_stream_id), _target['SYSTEM'], _target['TS'], int_id(_target['TGID']))
+                            continue
         
                         # Is this a new call stream on the target?
                         if (_stream_id not in _target_status):
