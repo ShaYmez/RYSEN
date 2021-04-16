250b0789b6728994f4c3b61f26a434ca93e876ef
Simon <simon@gb7fr.org.uk>
2021-04-16 10:49:02 +0100

    Revert "Make sure we always record last time, even if packet rejected by LoopControl"
    This reverts commit 94418256ec5ac5e61f56483d4d74c2c5f7923014.

--- bridge_master.py
+++ bridge_master.py
@@ -1272,11 +1272,6 @@ class routerOBP(OPENBRIDGE):
             
 
         if _call_type == 'group':
-            
-            
-            #Put this here as I think we risk timing out a stream too soon if it is rejected
-            self.STATUS[_stream_id]['LAST'] = pkt_time
-            
             # Is this a new call stream?
             if (_stream_id not in self.STATUS):
                 
@@ -1344,6 +1339,7 @@ class routerOBP(OPENBRIDGE):
                             return
 
 
+            self.STATUS[_stream_id]['LAST'] = pkt_time
             
             
             #Create STAT bridge for unknown TG
