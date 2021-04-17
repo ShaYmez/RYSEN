a41a6d1a41c5842dffb5c9cc496df0ea51e1f0ff
Simon <simon@gb7fr.org.uk>
2021-04-17 15:48:30 +0100

    try again

--- bridge_master.py
+++ bridge_master.py
@@ -1795,7 +1795,7 @@ class routerHBP(HBSYSTEM):
                             self.STATUS[_slot]['_bcsq'] = True
                         return
         
-            _int_seq = int(_seq)
+            _int_seq = _seq
             #Duplicate handling#
             #Duplicate complete packet
             if self.STATUS[_slot]['lastData'] and self.STATUS[_slot]['lastData'] == _data and _seq > 1:
