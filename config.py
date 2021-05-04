b77d2b72add07b4cf82bc2e94b48cf186c725629
Simon <simon@gb7fr.org.uk>
2021-05-04 21:18:50 +0100

    Add support for SERVER_ID

--- config.py
+++ config.py
@@ -124,7 +124,8 @@ def build_config(_config_file):
                     'TG2_ACL': config.get(section, 'TGID_TS2_ACL'),
                     'GEN_STAT_BRIDGES': config.getboolean(section, 'GEN_STAT_BRIDGES'),
                     'ALLOW_NULL_PASSPHRASE': config.getboolean(section, 'ALLOW_NULL_PASSPHRASE'),
-                    'ANNOUNCEMENT_LANGUAGE': config.get(section, 'ANNOUNCEMENT_LANGUAGE')
+                    'ANNOUNCEMENT_LANGUAGE': config.get(section, 'ANNOUNCEMENT_LANGUAGE'),
+                    'SERVER_ID': config.get(section, 'SERVER_ID')
                     
                 })
 
