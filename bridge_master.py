4f8f3e5a330935c3e637ba02e2c670c74fa65ad5
Simon <simon@gb7fr.org.uk>
2021-05-09 16:11:55 +0100

    argg

--- bridge_master.py
+++ bridge_master.py
@@ -2199,7 +2199,7 @@ if __name__ == '__main__':
     # Ident
     #This runs in a thread so as not to block the reactor
     ident_task = task.LoopingCall(threadIdent)
-    identa = ident_task.start(20)
+    identa = ident_task.start(900)
     identa.addErrback(loopingErrHandle)
     
     #Options parsing
