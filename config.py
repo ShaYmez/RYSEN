4ef08cbe5e8d1e7f25779d61c522818ae46e6348
Simon <simon@gb7fr.org.uk>
2021-05-30 16:15:25 +0100

    Fix error in tobytes()

--- config.py
+++ config.py
@@ -125,7 +125,7 @@ def build_config(_config_file):
                     'GEN_STAT_BRIDGES': config.getboolean(section, 'GEN_STAT_BRIDGES'),
                     'ALLOW_NULL_PASSPHRASE': config.getboolean(section, 'ALLOW_NULL_PASSPHRASE'),
                     'ANNOUNCEMENT_LANGUAGES': config.get(section, 'ANNOUNCEMENT_LANGUAGES'),
-                    'SERVER_ID': config.get(section, 'SERVER_ID').to_bytes(4, 'big')
+                    'SERVER_ID': config.getint(section, 'SERVER_ID').to_bytes(4, 'big')
                     
                 })
 
