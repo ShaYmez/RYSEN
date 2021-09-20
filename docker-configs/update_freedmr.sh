8fe8b36813cdb92fb62e31ca418c2542002e020b
Simon <simon@gb7fr.org.uk>
2021-09-20 19:38:02 +0100

    Update GPLv3 compliance - ensure files all have GPL header and coypright didaplyed. Tge project is licenced under this licence anyway but this is for completeness.
    GPLv3 is code is copyrighted code and should be correctly used and attributed.

--- docker-configs/update_freedmr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker pull hacknix/freedmr:latest && 
-docker container rm freedmr --force &&
-docker run --name=freedmr -d --read-only -v /etc/freedmr/freedmr.cfg:/opt/freedmr/freedmr.cfg \
--v /var/log/freedmr/freedmr.log:/opt/freedmr/freedmr.log \
--v /etc/freedmr/rules.py:/opt/freedmr/rules.py -p 62031:62031/udp -p 62036-62046:62036-62046/udp \
--p 4321:4321/tcp hacknix/freedmr:latest
-conntrack -F
