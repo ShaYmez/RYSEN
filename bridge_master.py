d810187498831bf1f1c47d10e0009c99459b5e0f
Simon <simon@gb7fr.org.uk>
2021-11-13 15:42:27 +0000

    Make safe for testing unit data via obp

--- bridge_master.py
+++ bridge_master.py
@@ -1413,45 +1413,7 @@ class routerOBP(OPENBRIDGE):
                     logger.info('(%s) *UNKNOWN DATA TYPE* STREAM ID: %s SUB: %s (%s) PEER: %s (%s) TGID %s (%s), TS %s', \
                             self._system, int_id(_stream_id), get_alias(_rf_src, subscriber_ids), int_id(_rf_src), get_alias(_peer_id, peer_ids), int_id(_peer_id), get_alias(_dst_id, talkgroup_ids), int_id(_dst_id), _slot)
             
-            #Send to all openbridges 
-            # We don't want to do this without more thought! 
-            # If we are going to do this is requires loop control 
-            # for data packets 
-            #
-            # Best for now to send all data packet to a single gateway 
-            
-            #for system in systems:
-                #if system  == self._system:
-                    #continue
-                #if CONFIG['SYSTEMS'][system]['MODE'] == 'OPENBRIDGE':
-                    #Assemble transmit HBP packet header
-                    #_tmp_data = b''.join([_data[:15], _tmp_bits.to_bytes(1, 'big'), _data[16:20]])
-                    #_tmp_data = b''.join([_tmp_data, dmrpkt])
-                    #systems[system].send_system(_tmp_data)
-                    #logger.info('(%s) UNIT Data Bridged to OBP System: %s DST_ID: %s', self._system, system,_int_dst_id)
-                    #if CONFIG['REPORTS']['REPORT']:
-                        #systems[system]._report.send_bridgeEvent('UNIT DATA,START,TX,{},{},{},{},{},{}'.format(system, int_id(_stream_id), int_id(_peer_id), int_id(_rf_src), 1, _int_dst_id).encode(encoding='utf-8', errors='ignore'))
-            
-            #This allow careful daisychaining to reach a DATA_GATEWAY
-            #Send UNIT data to data gateway
-            if CONFIG['GLOBAL']['DATA_GATEWAY'] and (CONFIG['GLOBAL']['DATA_GATEWAY'] in systems) \
-                and CONFIG['SYSTEMS'][CONFIG['GLOBAL']['DATA_GATEWAY']]['MODE'] == 'OPENBRIDGE':
-                    #Clear the TS bit -- all OpenBridge streams are effectively on TS1
-                    _tmp_bits = _bits & ~(1 << 7)
-                    #Assemble transmit HBP packet header
-                    _tmp_data = b''.join([_data[:15], _tmp_bits.to_bytes(1, 'big'), _data[16:20]])
-                    _tmp_data = b''.join([_tmp_data, dmrpkt])
-                    systems[CONFIG['GLOBAL']['DATA_GATEWAY']].send_system(_tmp_data)
-                    logger.info('(%s) UNIT Data Bridged to DATA_GATEWAY: %s DST_ID: %s', self._system,CONFIG['GLOBAL']['DATA_GATEWAY'],_int_dst_id)
-                    if CONFIG['REPORTS']['REPORT']:
-                        systems[system]._report.send_bridgeEvent('UNIT DATA,START,TX,{},{},{},{},{},{}'.format(system, int_id(_stream_id), int_id(_peer_id), int_id(_rf_src), 1, _int_dst_id).encode(encoding='utf-8', errors='ignore'))
-            else:
-                if not bool(CONFIG['GLOBAL']['DATA_GATEWAY']):
-                    logger.info('(%s) UNIT Data not Bridged - no DATA_GATEWAY: %s, DST_ID: %s',self._system,_int_dst_id)
-                elif CONFIG['GLOBAL']['DATA_GATEWAY'] not in systems:
-                    logger.warning('(%s) UNIT Data not Bridged - DATA_GATEWAY: %s not valid. DST_ID: %s',self._system, CONFIG['GLOBAL']['DATA_GATEWAY'],_int_dst_id)
-                elif CONFIG['SYSTEMS'][CONFIG['GLOBAL']['DATA_GATEWAY']]['MODE'] != 'OPENBRIDGE':
-                    logger.warning('(%s) UNIT Data not Bridged - DATA_GATEWAY: %s not OPENBRIDGE. DST_ID: %s',self._system, CONFIG['GLOBAL']['DATA_GATEWAY'],_int_dst_id)
+
                     
         if _call_type == 'group' or _call_type == 'vcsbk':
             # Is this a new call stream?
@@ -1926,7 +1888,7 @@ class routerHBP(HBSYSTEM):
                     #We only want to send data calls to individual IDs vis OpenBridge
                     if CONFIG['SYSTEMS'][system]['MODE'] == 'OPENBRIDGE' and _int_dst_id >= 1000000:
                         #Disabled in master for now 
-                        #self.sendDatatoOBP(system,_data,dmrpkt,pkt_time,_stream_id,_dst_id,_peer_id,_rf_src,_bits)
+                        self.sendDatatoOBP(system,_data,dmrpkt,pkt_time,_stream_id,_dst_id,_peer_id,_rf_src,_bits)
             
             #Send UNIT data to data gateway
             #if CONFIG['GLOBAL']['DATA_GATEWAY'] and (CONFIG['GLOBAL']['DATA_GATEWAY'] in systems) \
