fc56fa62a693c68997669dbf1e130a0b2061bbdf
Simon <simon@gb7fr.org.uk>
2022-01-05 17:53:19 +0000

    df;ld

--- bridge_master.py
+++ bridge_master.py
@@ -1522,7 +1522,7 @@ class routerOBP(OPENBRIDGE):
                     call_duration = pkt_time - self.STATUS[_stream_id]['START']
                     packet_rate = 0
                     if 'packets' in self.STATUS[_stream_id]:
-                    packet_rate = self.STATUS[_stream_id]['packets'] / call_duration
+                        packet_rate = self.STATUS[_stream_id]['packets'] / call_duration
                     logger.warning("(%s) OBP UNIT *LoopControl* FIRST OBP %s, STREAM ID: %s, TG %s, IGNORE THIS SOURCE. PACKET RATE %0.2f/s",self._system, fi, int_id(_stream_id), int_id(_dst_id),packet_rate)
                     self.STATUS[_stream_id]['LOOPLOG'] = True
                 self.STATUS[_stream_id]['LAST'] = pkt_time
