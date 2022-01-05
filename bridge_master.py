f947206556a55a6ffb75641c4493fe552c8cc19a
Simon <simon@gb7fr.org.uk>
2022-01-05 16:15:17 +0000

    2 dp

--- bridge_master.py
+++ bridge_master.py
@@ -1740,7 +1740,7 @@ class routerOBP(OPENBRIDGE):
                 packet_rate = 100
                 if 'packets' in self.STATUS[_stream_id]:
                     packet_rate = self.STATUS[_stream_id]['packets'] / call_duration
-                logger.info('(%s) *CALL END*   STREAM ID: %s SUB: %s (%s) PEER: %s (%s) TGID %s (%s), TS %s, Duration: %.2f, Packet rate: %s', \
+                logger.info('(%s) *CALL END*   STREAM ID: %s SUB: %s (%s) PEER: %s (%s) TGID %s (%s), TS %s, Duration: %.2f, Packet rate: %.2f', \
                         self._system, int_id(_stream_id), get_alias(_rf_src, subscriber_ids), int_id(_rf_src), get_alias(_peer_id, peer_ids), int_id(_peer_id), get_alias(_dst_id, talkgroup_ids), int_id(_dst_id), _slot, call_duration, packet_rate)
                 if CONFIG['REPORTS']['REPORT']:
                    self._report.send_bridgeEvent('GROUP VOICE,END,RX,{},{},{},{},{},{},{:.2f}'.format(self._system, int_id(_stream_id), int_id(_peer_id), int_id(_rf_src), _slot, int_id(_dst_id), call_duration).encode(encoding='utf-8', errors='ignore'))
