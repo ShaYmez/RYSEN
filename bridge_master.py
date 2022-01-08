b03b342227b81e7a5af32ab597b7173d2ae4318e
Simon <simon@gb7fr.org.uk>
2022-01-08 12:32:03 +0000

    debug

--- bridge_master.py
+++ bridge_master.py
@@ -1713,7 +1713,7 @@ class routerOBP(OPENBRIDGE):
                     return
                 #Duplicate DMR payload to previuos packet (by CRC16)
                 if _pkt_crc in self.STATUS[_stream_id]['crcs']:
-                    logger.warning("(%s) *PacketControl* DMR packet payload with CRC16: %s seen before in this stream, disgarding. Stream ID:, %s TGID: %s",self._system,_pkt_crc,int_id(_stream_id),int_id(_dst_id))
+                    logger.warning("(%s) *PacketControl* DMR packet payload with CRC16: %s seen before in this stream, disgarding. Stream ID:, %s TGID: %s, crcs: %s",self._system,_pkt_crc,int_id(_stream_id),int_id(_dst_id),self.STATUS[_stream_id]['crcs'])
                     return
                 #Inbound out-of-order packets
                 if _seq and self.STATUS[_stream_id]['lastSeq']  and (_seq != 1) and (_seq < self.STATUS[_stream_id]['lastSeq']):
