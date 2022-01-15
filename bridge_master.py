94c8957434dced699a6183b35acaf1317d4c9f34
Simon <simon@gb7fr.org.uk>
2022-01-15 15:35:12 +0000

    fix bad type in options

--- bridge_master.py
+++ bridge_master.py
@@ -791,7 +791,7 @@ def options_config():
                     if 'DEFAULT_UA_TIMER' not in _options:
                         _options['DEFAULT_UA_TIMER'] = CONFIG['SYSTEMS'][_system]['DEFAULT_UA_TIMER']
                     
-                    if 'VOICE' in _options and (CONFIG['SYSTEMS'][_system]['VOICE_IDENT'] != bool(int(_options['VOICE']))):
+                    if 'VOICE' in _options and isinstance(_options['VOICE'],int) and (CONFIG['SYSTEMS'][_system]['VOICE_IDENT'] != bool(int(_options['VOICE']))):
                         CONFIG['SYSTEMS'][_system]['VOICE_IDENT'] = bool(int(_options['VOICE']))
                         logger.debug("(OPTIONS) %s - Setting voice ident to %s",_system,CONFIG['SYSTEMS'][_system]['VOICE_IDENT'])
                         
