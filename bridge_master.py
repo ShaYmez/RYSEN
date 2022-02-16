301d22123fe2d1997b56de69c78d44c0e053b430
Simon <simon@gb7fr.org.uk>
2022-02-16 02:50:27 +0000

    Revert "Source data for unit data packets"
    This reverts commit ae76dd7c4fa7c71dfec84b7259d73883debcf6ff.

--- bridge_master.py
+++ bridge_master.py
@@ -2168,7 +2168,7 @@ class routerHBP(HBSYSTEM):
                     continue
                 #We only want to send data calls to individual IDs via OpenBridge
                 if CONFIG['SYSTEMS'][system]['MODE'] == 'OPENBRIDGE' and CONFIG['SYSTEMS'][system]['VER'] > 1 and (_int_dst_id >= 1000000):
-                    self.sendDataToOBP(system,_data,dmrpkt,pkt_time,_stream_id,_dst_id,_peer_id,_rf_src,_bits,_slot,_ber,_rssi,_source_server)
+                    self.sendDataToOBP(system,_data,dmrpkt,pkt_time,_stream_id,_dst_id,_peer_id,_rf_src,_bits,_slot)
 
             
             #If destination ID is in the Subscriber Map
