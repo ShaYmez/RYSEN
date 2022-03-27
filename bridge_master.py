1365849d20d1c025aa63dc5bfe69be46e7862cf9
Simon <simon@gb7fr.org.uk>
2022-03-27 23:56:01 +0100

    typo

--- bridge_master.py
+++ bridge_master.py
@@ -1598,8 +1598,8 @@ class routerOBP(OPENBRIDGE):
             
             #Send all data to DATA-GATEWAY if enabled and valid
             if CONFIG['GLOBAL']['DATA_GATEWAY'] and 'DATA-GATEWAY' in CONFIG['SYSTEMS'] and CONFIG['SYSTEMS']['DATA-GATEWAY']['MODE'] == 'OPENBRIDGE' and CONFIG['SYSTEMS']['DATA-GATEWAY']['ENABLED']:
-                logger.info('(%s) DATA packet sent to DATA-GATEWAY, System: %s',self._system, _d_system)
-                self.sendDataToOBP('DATA-GATEWAY',_data,dmrpkt,pkt_time,_stream_id,_dst_id,_peer_id,_rf_src,_bits,_slot,_source_rptr)
+                logger.info('(%s) DATA packet sent to DATA-GATEWAY',self._system)
+                self.sendDataToOBP('DATA-GATEWAY',_data,dmrpkt,pkt_time,_stream_id,_dst_id,_peer_id,_rf_src,_bits,_slot,_source_rptr,_ber,_rssi)
                  
             
             #Send other openbridges
@@ -2204,7 +2204,7 @@ class routerHBP(HBSYSTEM):
             
             #Send all data to DATA-GATEWAY if enabled and valid
             if CONFIG['GLOBAL']['DATA_GATEWAY'] and 'DATA-GATEWAY' in CONFIG['SYSTEMS'] and CONFIG['SYSTEMS']['DATA-GATEWAY']['MODE'] == 'OPENBRIDGE' and CONFIG['SYSTEMS']['DATA-GATEWAY']['ENABLED']:
-                logger.info('(%s) DATA packet sent to DATA-GATEWAY, System: %s',self._system, _d_system)
+                logger.info('(%s) DATA packet sent to DATA-GATEWAY',self._system)
                 self.sendDataToOBP('DATA-GATEWAY',_data,dmrpkt,pkt_time,_stream_id,_dst_id,_peer_id,_rf_src,_bits,_slot,_source_rptr)
                 
             #Send to all openbridges 
