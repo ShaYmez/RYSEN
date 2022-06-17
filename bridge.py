0752c1eaaa338f1bd418ef44099f0393b625c6d1
M0VUB <76499782+ShaYmez@users.noreply.github.com>
2022-06-17 22:39:55 +0100

    Update bridge.py
    Typo

--- bridge.py
+++ bridge.py
@@ -953,7 +953,7 @@ if __name__ == '__main__':
     CONFIG['_LOCAL_SUBSCRIBER_IDS'] = local_subscriber_ids
     CONFIG['_SERVER_IDS'] = server_ids
     
-    # Import the ruiles file as a module, and create BRIDGES from it
+    # Import the rules file as a module, and create BRIDGES from it
     spec = importlib.util.spec_from_file_location("module.name", cli_args.RULES_FILE)
     rules_module = importlib.util.module_from_spec(spec)
     try:
