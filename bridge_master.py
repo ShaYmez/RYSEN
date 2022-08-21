55db2b0e0712e4dd48227624c198a9f481c1eeb3
ShaYmez <support@gb7nr.co.uk>
2022-08-21 21:46:36 +0100

    RYSEN replace

--- bridge_master.py
+++ bridge_master.py
@@ -3046,7 +3046,7 @@ if __name__ == '__main__':
                 words[lang][_mapword] = words[lang][_map[_mapword]]
 
     # HBlink instance creation
-    logger.info('(GLOBAL) FreeDMR \'bridge_master.py\' -- SYSTEM STARTING...')
+    logger.info('(GLOBAL) RYSEN \'bridge_master.py\' -- SYSTEM STARTING...')
 
     
     listeningPorts = {}
