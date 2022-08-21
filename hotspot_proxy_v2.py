90af831a90a9b2d86d032ca24731b535f16dd5c2
ShaYmez <support@gb7nr.co.uk>
2022-08-21 20:48:11 +0100

    Merge upstream

--- hotspot_proxy_v2.py
+++ hotspot_proxy_v2.py
@@ -1,4 +1,4 @@
-# RYSEN DMRMaster+ Version 1.3.4 
+# RYSEN DMRMaster+ Version 1.3.8 
 #
 ###############################################################################
 # Copyright (C) 2020 Simon Adlem, G7RZU <g7rzu@gb7fr.org.uk>  
@@ -237,13 +237,13 @@ if __name__ == '__main__':
 
     # CLI argument parser - handles picking up the config file from the command line, and sending a "help" message
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', action='store', dest='CONFIG_FILE', help='/full/path/to/config.file (usually rysen.cfg)')
+    parser.add_argument('-c', '--config', action='store', dest='CONFIG_FILE', help='/full/path/to/config.file (usually freedmr.cfg)')
     cli_args = parser.parse_args()
 
 
     # Ensure we have a path for the config file, if one wasn't specified, then use the execution directory
     if not cli_args.CONFIG_FILE:
-        cli_args.CONFIG_FILE = os.path.dirname(os.path.abspath(__file__))+'/rysen.cfg'
+        cli_args.CONFIG_FILE = os.path.dirname(os.path.abspath(__file__))+'/freedmr.cfg'
     
     _config_file = cli_args.CONFIG_FILE
     
