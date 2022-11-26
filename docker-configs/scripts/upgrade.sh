a26f81048d092da29147a0ca75b38e3128f408e2
M0VUB <76499782+ShaYmez@users.noreply.github.com>
2022-11-26 19:31:46 +0000

    Update upgrade.sh
    update upgrade sh

--- docker-configs/scripts/upgrade.sh
+++ docker-configs/scripts/upgrade.sh
@@ -1,1 +1,20 @@
+# SYSTEM-X FORMERLY RYSEN MASTER+ (HBlink3) A FORK OF THE FREEDMR / HBLINK PROJECT
+# This script written by Shane Daley M0VUB. The script gracefully shutsdown services while services are cleaned and logs are truncated.
+# We can also add items in this script for future use like updates or further log trims.
+# Add to the cron tab for auto execution
 
+#   Copyright (C) 2020 Shane P, Daley  M0VUB <support@gb7nr.co.uk>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+# Full upgrade of RYSEN Master+ / service clean/truncate/ and restart of services..
+
+echo "No upgrades available"
